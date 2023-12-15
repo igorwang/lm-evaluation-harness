315a8edbac45aaac1a21e1435319590c67447951
igorwang <wangyi@techower.com>
2023-12-15 13:34:33 +0800

    f1_micro

--- lm_eval/tasks/acl_arc/utils.py
+++ lm_eval/tasks/acl_arc/utils.py
@@ -11,11 +11,33 @@
 -------------------------------------------------
 """
 import datasets
+import sklearn.metrics
+from lm_eval.api.registry import register_metric, register_aggregation
+import numpy as np
+
+
+@register_aggregation('f1_micro')
+def f1_micro(items):
+    unzipped_list = list(zip(*items))
+    golds = unzipped_list[0]
+    preds = unzipped_list[1]
+    fscore = sklearn.metrics.f1_score(golds, preds, average='micro')
+    return np.max(fscore)
 
 
 def process_docs(dataset: datasets.Dataset):
     def _process(doc):
-
         return doc
 
     return dataset.map(_process)
+
+
+def process_results(doc, results):
+    import re
+    intents = ['background', 'uses', 'compares', 'motivation', 'continuation', 'future', 'unknow']
+    pattern = '|'.join(intents)
+    gold = doc['intent']
+    pred = re.findall(pattern, results[0])
+    pred = pred[0] if pred else "unknow"
+    pred = intents.index(pred)
+    return {"f1": (gold, pred), "acc": 1 if gold == pred else 0}
