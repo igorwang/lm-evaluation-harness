9cbf6d54da60bc9117562a01f2e154501404f519
igorwang <wangyi@techower.com>
2023-12-15 13:44:52 +0800

    f1

--- lm_eval/tasks/acl_arc/utils.py
+++ lm_eval/tasks/acl_arc/utils.py
@@ -16,13 +16,27 @@ from lm_eval.api.registry import register_metric, register_aggregation
 import numpy as np
 
 
-@register_aggregation('f1_micro')
-def f1_micro(items):
+def f1(items, average='macro'):
     unzipped_list = list(zip(*items))
     golds = unzipped_list[0]
     preds = unzipped_list[1]
-    fscore = sklearn.metrics.f1_score(golds, preds, average='micro')
+    fscore = sklearn.metrics.f1_score(golds, preds, average='macro')
     return np.max(fscore)
+
+
+@register_aggregation('f1_macro')
+def f1_macro(items):
+    return f1(items, average='macro')
+
+
+@register_aggregation('f1_micro')
+def f1_micro(items):
+    return f1(items, average='micro')
+
+
+@register_aggregation('f1_weighted')
+def f1_weighted(items):
+    return f1(items, average='weighted')
 
 
 def process_docs(dataset: datasets.Dataset):
@@ -37,7 +51,7 @@ def process_results(doc, results):
     intents = ['background', 'uses', 'compares', 'motivation', 'continuation', 'future', 'unknow']
     pattern = '|'.join(intents)
     gold = doc['intent']
-    pred = re.findall(pattern, results[0])
+    pred = re.findall(pattern, results[0].lower())
     pred = pred[0] if pred else "unknow"
     pred = intents.index(pred)
     return {"f1": (gold, pred), "acc": 1 if gold == pred else 0}
